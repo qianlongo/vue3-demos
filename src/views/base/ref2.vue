f32a319d510523ae21e407df8c732982524dc258
qianlongo <1162633511@qq.com>
2022-04-16 18:00:57 +0800

    feat: $ref语法糖

---
+++ src/views/base/ref2.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="ref2">
+    {{ count }}    
+  </div>
+</template>
+<script setup>
+// $ref可以减少.value的操作
+import { $ref } from 'vue'
+const count = $ref(1)
+
+
+setInterval(() => {
+  count++
+}, 1000)
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack: (config) => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((options) => {
+        return {
+          ...options,
+          reactivityTransform: true
+        }
+      })
+  }
 })
