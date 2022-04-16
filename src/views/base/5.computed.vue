9c334266a3e0c791b2d163861b555b23911a2ff1
qianlongo <1162633511@qq.com>
2022-04-16 18:07:51 +0800

    feat: 修改文件命名

---
+++ src/views/base/5.computed.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="computed">
+    {{ sum }}
+  </div>
+</template>
+
+<script setup>
+import { computed, $ref } from 'vue'
+
+const a = $ref(0)
+const b = $ref(1)
+const sum = computed(() => {
+  return a + b + 10
+})
+
+setInterval(() => {
+  a++
+  b++
+}, 1000)
+</script>
