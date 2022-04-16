e3f97003f4bb4ba88dda24340432a65b97460369
qianlongo <1162633511@qq.com>
2022-04-16 18:24:26 +0800

    feat: watch

---
+++ src/views/base/8.watch.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="watch">{{ count }}</div>
+</template>
+
+<script setup>
+import { ref, $ref, watch } from 'vue'
+// 貌似这样写 watch会监听不到
+let count = $ref(1)
+let count2 = ref(1)
+
+watch(count2, async(newVal, val) => {
+  console.log(newVal, val)
+})
+
+setInterval(() => {
+  count++
+  count2.value++
+}, 1000)
+</script>
