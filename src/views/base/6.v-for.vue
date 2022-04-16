81ee3aac5250ddf0abfe9d0809351dc14b979066
qianlongo <1162633511@qq.com>
2022-04-16 18:11:34 +0800

    feat: v-for

---
+++ src/views/base/6.v-for.vue
@@ -0,0 +1,21 @@
+<template>
+<ul class="v-for">
+  <li
+    v-for="(it, i) in objectFor"
+    :key="i"
+  >
+    {{ it }}
+  </li>
+</ul>
+</template>
+<script setup>
+import { $ref } from 'vue'
+
+const objectFor = $ref([
+  'fatfish'
+])
+
+objectFor.push('fatfish2')
+
+objectFor = [ '1' ]
+</script>
