97c3560a2ff68bfb4a8c1d326d9eac81198a38e0
qianlongo <1162633511@qq.com>
2022-04-16 16:18:04 +0800

    feat: base/reactive

---
+++ src/views/base/reactive.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="reactive">
+    <button @click="increment">{{ state.count }}</button>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+
+const state = reactive({ count: 0 })
+const increment = () => {
+  state.count++
+}
+</script>
