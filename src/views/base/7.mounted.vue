b192f780aa750f3625f581c6e3ac684068358ef3
qianlongo <1162633511@qq.com>
2022-04-16 18:14:52 +0800

    feat: 生命周期

---
+++ src/views/base/7.mounted.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mounted"></div>
+</template>
+
+<script setup>
+  import { onMounted, onBeforeUnmount } from 'vue'
+
+  onMounted(() => {
+    console.log('onMounted')
+  })
+
+  onBeforeUnmount(() => {
+    console.log('onBeforeUnmount')
+  })
+</script>
