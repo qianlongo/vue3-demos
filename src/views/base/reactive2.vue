2896d42c42b8fd7c596d75d0fc2531f5a26b0d00
qianlongo <1162633511@qq.com>
2022-04-16 16:22:06 +0800

    feat: react2

---
+++ src/views/base/reactive2.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="reactive2">
+    {{ obj.nested.count }}
+    {{ obj.arr }}
+  </div>
+</template>
+
+<script setup>
+  import { reactive } from 'vue'
+
+  const obj = reactive({
+    nested: {
+      count: 0
+    },
+    arr: [ 'foo', 'bar' ]
+  })
+  // 深度响应式
+  obj.nested.count++
+  obj.arr.push('fatfish')
+</script>
